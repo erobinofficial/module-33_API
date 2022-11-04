ac57a28e0ea9d7774f7c2cd9673da70ec1c67487
erobinofficial <erobin8778@gmail.com>
2022-11-04 18:13:48 +0600

    api with css

---
+++ posts.js
@@ -0,0 +1,19 @@
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+     .then(data => displayPosts(data))    
+}
+loadPosts();
+
+function displayPosts(posts){
+    const section = document.getElementById('posts');
+    for(const post of posts){
+        const div = document.createElement('div');
+        div.classList.add('post');
+        div.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        `;
+        section.appendChild(div);
+    }
+}
